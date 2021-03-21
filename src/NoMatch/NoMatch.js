66cfeeba40f3850bef1b5e304510eb12e9faea1a
shahidul jahid <shahiduljahid3@gmail.com>
2021-03-21 20:44:16 +0800

    sij

--- src/NoMatch/NoMatch.js
+++ src/NoMatch/NoMatch.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const NoMatch = () => {
-    return (
-        <div>
-            <h1>nothing found</h1>
-            <p>404 erorr</p>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>nothing found</h1>
+      <p>404 erorr</p>
+    </div>
+  );
 };
 
 export default NoMatch;
